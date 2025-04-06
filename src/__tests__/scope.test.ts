99c345353e36bb950274dbd4280346ff04e25353
wxzhang <wxzhang@126.com>
2025-04-06 15:59:17 +0800

    update

--- src/__tests__/scope.test.ts
+++ src/__tests__/scope.test.ts
@@ -17,7 +17,21 @@ describe("scope", ()=>{
         expect(events).toEqual(["x","a/b/c/x"])
     })
 
+    test("scope简单的退订事件",()=>{
+        const emitter = new FastEvent() 
+        const scope = emitter.scope("a/b/c")
 
+        const events:string[] =[]
+        const listener = (payload:any,type:any)=>{
+            events.push(type)
+        }
+        const subscriber =scope.on("x",listener)
+        emitter.on("a/b/c/x",listener)
+        scope.emit("x",1)        
+        subscriber.off()
+        scope.emit("x",1)      
+        expect(events).toEqual(["x","a/b/c/x","a/b/c/x"])
+    })
 })
 
  
