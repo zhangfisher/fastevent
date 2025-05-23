426f78168f7420cd431b7731098711efdf564d0a
wxzhang <wxzhang@126.com>
2025-05-23 16:00:53 +0800

    feat: add channel

---
+++ packages/native/src/channel.ts
@@ -0,0 +1,20 @@
+/**
+ * 
+ * 
+ *  const emitter = new FastEvent()
+ * 
+ * 
+ *  channel = emitter.channel("a.b.c.d", *(channel)=>{
+ *      
+ *  })
+ * 
+ *   emitter.on("a.b.c.d",*(channel) => {
+ *      while(true){
+ *         const value = yield channel.pop(1)
+ *      }
+ *   })
+ * 
+ */
+
+
+export {}
