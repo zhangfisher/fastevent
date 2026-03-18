bf7588966b6f19e7305d6ca2d0f11608e35ee496
wxzhang <wxzhang@126.com>
2026-03-18 18:01:02 +0800

    update

--- packages/native/src/__benchmarks__/emit.ts
+++ packages/native/src/__benchmarks__/emit.ts
@@ -1,30 +1,26 @@
 // oxlint-disable no-unused-vars
-import { createCallProfiler } from "../utils/profiler";
+import { CallProfiler } from "../utils/callProfiler";
 import { FastEvent } from "../event";
-const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
 const emitter = new FastEvent();
 
-const profiler = createCallProfiler(emitter);
+const profiler = new CallProfiler(emitter);
 
 async function test() {
     let count: number = 0;
     await profiler.run(
         async () => {
-            // return new Promise<void>(async (resolve) => {
             const { off } = emitter.on("test", async () => {
                 count++;
-                // await delay(1);
             });
             await emitter.emitAsync("test", 1);
             off();
-            // });
         },
         {
             executionCount: 100,
         },
     );
-    console.log(profiler.toTree());
+    console.log(profiler.render());
 }
 
 test().then(() => {
